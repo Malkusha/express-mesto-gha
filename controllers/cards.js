5570bf4ca8eceb8237dd07acb01618729e5ae67b
Pashkova <malkusha@mail.ru>
2023-08-07 01:16:55 +0300

    card next

--- controllers/cards.js
+++ controllers/cards.js
@@ -7,7 +7,7 @@ const {
   AccessError
 } = require("../errors/index");
 
-function getCards(req, res) {
+function getCards(req, res, next) {
   Card.find({})
     .then((cards) => res.status(200).send({ data: cards }))
     .catch((err) => {
@@ -15,7 +15,7 @@ function getCards(req, res) {
     })
 }
 
-function createCard(req, res) {
+function createCard(req, res, next) {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user._id })
     .then((card) => res.status(201).send({ data: card }))
@@ -27,7 +27,7 @@ function createCard(req, res) {
     });
 }
 
-function deleteCardById(req, res) {
+function deleteCardById(req, res, next) {
   const { cardId } = req.params;
   Card.findByIdAndRemove(cardId)
     .then((card) => {
@@ -44,7 +44,7 @@ function deleteCardById(req, res) {
     });
 }
 
-function setLike(req, res) {
+function setLike(req, res, next) {
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
@@ -66,7 +66,7 @@ function setLike(req, res) {
     });
 }
 
-function removeLike(req, res) {
+function removeLike(req, res, next) {
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $pull: { likes: req.user._id } }, // убрать _id из массива
